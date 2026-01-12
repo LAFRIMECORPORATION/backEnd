389394d0b829f8050e85e853549b9f8f4cae2348
lafrimecorporation <merimesiewe84@gmail.com>
2026-01-12 20:08:39 +0100

    'update3'

--- pool.js
+++ pool.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    connectionString: process.env.DB_URL,
+    connectionString: 'postgresql://neondb_owner:npg_RkLl89ZbhVpo@ep-crimson-dawn-ad3xdlas-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
     ssl: {
         rejectUnauthorized: false
     }
