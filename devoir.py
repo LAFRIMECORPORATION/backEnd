8d6e559ceb78b05aef8ca125f641146c283750d9
lafrimecorporation <merimesiewe84@gmail.com>
2026-03-07 13:11:09 +0100

    'update80'

---
+++ devoir.py
@@ -0,0 +1,28 @@
+import math 
+class Point:
+    def __init__(self, x=0, y=0)
+    self.x = x
+    self.y = y
+
+
+    def afficher(self):
+        print(f"({self.x},{self.y})")
+
+
+        
+
+def deplacer(self, dx, dy):
+    self.x += dx
+    self.y += dy
+
+
+
+def distance(self, autre_point):
+    return math.sqrt((self.x - autre_point.x)**2 +(self.y - autre_point.y)**2)
+
+
+def translation (self, u, v):
+    self.x += u
+    self.y += v    
+
+
